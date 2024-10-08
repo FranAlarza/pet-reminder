4ae2d25dd5b0a61c8943dcb14f669f93d37493dd
Fran Alarza <franalarza@gmail.com>
2024-10-08 19:54:05 +0200

    first commit

---
+++ PetReminder/PetReminder/PetReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetReminderApp.swift
+//  PetReminder
+//
+//  Created by Fran Alarza on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PetReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetReminder/PetReminderTests/PetReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetReminderTests.swift
+//  PetReminderTests
+//
+//  Created by Fran Alarza on 8/10/24.
+//
+
+import Testing
+@testable import PetReminder
+
+struct PetReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
