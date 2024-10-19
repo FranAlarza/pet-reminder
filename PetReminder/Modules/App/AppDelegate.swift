9ae929c230ff97016e7386f3d3081386d5c3b06a
Fran Alarza <franalarza@gmail.com>
2024-10-19 20:51:21 +0200

    feat: add settings with shake

--- PetReminder/Modules/App/AppDelegate.swift
+++ PetReminder/Modules/App/AppDelegate.swift
@@ -10,9 +10,11 @@ import UIKit
 import FirebaseCore
 import FirebaseAnalytics
 import FirebaseAuth
+import Shake
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      Shake.start(apiKey: "cGkuIC8HPfcu2RYOXzuHOUSY1YldwQb7meKUQIVICLO65V7Qmnqh9om")
       FirebaseApp.configure()
     return true
   }
--- PetReminder/Modules/Settings/SettingsScreen.swift
+++ PetReminder/Modules/Settings/SettingsScreen.swift
@@ -6,10 +6,60 @@
 //
 
 import SwiftUI
+import StoreKit
+import Shake
 
 struct SettingsScreen: View {
+    @Environment(\.requestReview) var requestReview
     var body: some View {
-        Text("Settings Screen")
+        ZStack {
+            VStack(alignment: .center) {
+                List {
+                    Section("INFO") {
+                        Button {
+                            requestReview.callAsFunction()
+                        } label: {
+                            HStack(spacing: 16) {
+                                Image(systemName: "star.fill")
+                                Text("Rate Us")
+                                    .foregroundStyle(.primary)
+                                    .font(.body)
+                                Spacer()
+
+                            }
+                        }
+                        
+                        Button {
+                            Shake.show()
+                        } label: {
+                            HStack(spacing: 20) {
+                                Image(systemName: "lightbulb.fill")
+                                Text("Request New Feature")
+                                    .foregroundStyle(.primary)
+                                    .font(.body)
+                                Spacer()
+                            }
+                        }
+                    }
+                    
+                }
+                .listStyle(.insetGrouped)
+            }
+            HStack(spacing: 4) {
+                Text("Made with")
+                
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.red)
+                
+                Text("by")
+                
+                Link("Fran Alarza", destination: URL(string: "https://www.linkedin.com/in/francisco-javier-alarza-sanchez/")!)
+                    .foregroundColor(.blue)
+                    .bold()
+            }
+            .offset(y: -UIScreen.main.bounds.height * 0.25)
+            .font(.body)
+        }
     }
 }
 
